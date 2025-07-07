678bec2de01e7585a30c8581e1672ebe9a2aec66
Hakan özkale <hakanozkalemkp@gmail.com>
2025-07-07 16:51:55 +0300

    images quality improved

--- src/app/use-cases/product-adoption/page.js
+++ src/app/use-cases/product-adoption/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import Navbar from '../../components/Navbar.js';
+import Navbar from '../../components/Navbar';
 import Image from 'next/image';
 import { CursorSvg } from '../../utils/showcase/svgShowcase';
 import { useEffect, useRef, useState } from 'react';
